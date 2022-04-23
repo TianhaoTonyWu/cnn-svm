69837e1aea1b658cd0e2baf77268a6bf5834fbae
TianhaoTonyWu <tianhao.wu@mail.mcgill.ca>
2022-04-23 13:58:29 -0400

    Update model3.py

--- model/model3.py
+++ model/model3.py
@@ -56,8 +56,8 @@ class CNNSVM3:
                 )
 
             # First convolutional layer
-            first_conv_weight = self.weight_variable([5, 5, 1, 32])
-            first_conv_bias = self.bias_variable([32])
+            first_conv_weight = self.weight_variable([5, 5, 32, 64])
+            first_conv_bias = self.bias_variable([64])
 
             input_image = tf.reshape(x_input, [-1, 28, 28, 1])
 
